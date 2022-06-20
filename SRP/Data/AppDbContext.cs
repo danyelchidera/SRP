db4824e5ebcf23d5557334efc924f2c80b3442a3
danyelchidera <danyelchidera1@gmail.com>
2022-06-20 22:10:03 +0100

    Add project files.

---
+++ SRP/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SRP.Models;
+
+namespace SRP.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ SRP/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SRP.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+        public int Amount { get; set; }
+    }
+}
